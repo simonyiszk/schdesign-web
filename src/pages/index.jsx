fcef64e076129d690507af81c42ec8ac934e7f42
Kristóf Poduszló <kripod@protonmail.com>
2018-06-01 13:27:43 +0200

    Add introduction text to the about section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import Container from '../components/Container';
 import Layout from '../components/Layout';
+import styles from './index.module.scss';
 
 const IndexPage = () => (
   <Layout>
     <Container>
-      <h1>Schönherz Design Stúdió</h1>
+      <h1 className={styles.sectionTitle}>Rólunk</h1>
+
+      <p>
+        A Schönherz Design Stúdió a Simonyi Károly Szakkollégium kreatív
+        alkotóműhelye.
+      </p>
+
+      <p>
+        Bármilyen designnal, arculattervezéssel kapcsolatos feladattal szívesen
+        foglalkozunk, legyen az egy weboldal, egy plakát vagy akár egy mobil
+        alkalmazás megtervezése.
+      </p>
+
+      <p>
+        Keress minket projektmunkákkal, vagy nyerj betekintést a világunkba
+        workshopjaink alkalmával!
+      </p>
     </Container>
   </Layout>
 );
