419848468545b1ae82eddc67a84e2a4e345c167f
Kristóf Poduszló <kripod@protonmail.com>
2018-06-01 11:53:59 +0200

    Specify site typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
     },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
     'gatsby-transformer-yaml',
   ],
 };
