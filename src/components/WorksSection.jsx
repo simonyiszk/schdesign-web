820d42120fe0d50da1437201a9d0f0d329875679
Kristóf Poduszló <kripod@protonmail.com>
2018-07-19 00:35:22 +0200

    Add lightbox for the gallery of works

--- src/components/WorksSection.jsx
+++ src/components/WorksSection.jsx
@@ -3,21 +3,39 @@ import React from 'react';
 import Measure from 'react-measure';
 import Gallery from 'react-photo-gallery';
 import Container from './Container';
+import Lightbox from './Lightbox';
 import SectionTitle from './SectionTitle';
 import styles from './WorksSection.module.scss';
 
 export default class WorksSection extends React.PureComponent {
   constructor(props) {
     super(props);
-
     this.state = {
+      isLightboxOpen: false,
+      initialLightboxImage: 0,
       width: 0,
     };
+
+    this.openLightbox = this.openLightbox.bind(this);
+    this.closeLightbox = this.closeLightbox.bind(this);
+  }
+
+  openLightbox(event, obj) {
+    this.setState({
+      isLightboxOpen: true,
+      initialLightboxImage: obj.index,
+    });
+  }
+
+  closeLightbox() {
+    this.setState({
+      isLightboxOpen: false,
+    });
   }
 
   render() {
     const { ...props } = this.props;
-    const { width } = this.state;
+    const { isLightboxOpen, initialLightboxImage, width } = this.state;
 
     return (
       <section {...props}>
@@ -46,45 +64,60 @@ export default class WorksSection extends React.PureComponent {
             }
 
             return (
-              <div ref={measureRef} className={styles.galleryContainer}>
-                <StaticQuery
-                  query={graphql`
-                    query WorksSectionQuery {
-                      allWorksYaml(limit: 12) {
-                        edges {
-                          node {
-                            name
-                            author
-                            image {
-                              childImageSharp {
-                                fluid(maxWidth: 1024, quality: 92) {
-                                  src
-                                  srcSet
-                                  sizes
-                                  aspectRatio
-                                }
+              <StaticQuery
+                query={graphql`
+                  query WorksSectionQuery {
+                    allWorksYaml(limit: 12) {
+                      edges {
+                        node {
+                          name
+                          author
+                          image {
+                            childImageSharp {
+                              fluid(maxWidth: 1024, quality: 92) {
+                                src
+                                srcSet
+                                sizes
+                                aspectRatio
                               }
                             }
                           }
                         }
                       }
                     }
-                  `}
-                  render={data => (
-                    <Gallery
-                      photos={data.allWorksYaml.edges
-                        .map(({ node }) => node.image.childImageSharp.fluid)
-                        .map(({ aspectRatio, ...rest }) => ({
-                          width: aspectRatio,
-                          height: 1,
-                          ...rest,
-                        }))}
-                      columns={columns}
-                      margin={0}
-                    />
-                  )}
-                />
-              </div>
+                  }
+                `}
+                render={data => {
+                  const images = data.allWorksYaml.edges
+                    .map(({ node }) => ({
+                      ...node.image.childImageSharp.fluid,
+                      caption: `${node.name} – ${node.author}`,
+                    }))
+                    .map(({ aspectRatio, ...rest }) => ({
+                      ...rest,
+                      width: aspectRatio,
+                      height: 1,
+                    }));
+
+                  return (
+                    <div ref={measureRef} className={styles.galleryContainer}>
+                      <Gallery
+                        photos={images}
+                        columns={columns}
+                        margin={0}
+                        onClick={this.openLightbox}
+                      />
+
+                      <Lightbox
+                        images={images}
+                        isOpen={isLightboxOpen}
+                        initialImage={initialLightboxImage}
+                        onClose={this.closeLightbox}
+                      />
+                    </div>
+                  );
+                }}
+              />
             );
           }}
         </Measure>
