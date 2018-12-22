717caa4c1d7dac167e8592d075cf4aa47e1ecf8e
Kristóf Poduszló <kripod@protonmail.com>
2018-12-23 00:36:29 +0100

    Change default bold text weight

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -4,6 +4,7 @@ const typography = new Typography({
   headerFontFamily: ['Roboto Mono', 'monospace'],
   headerWeight: 500,
   bodyFontFamily: ['Montserrat', 'sans-serif'],
+  boldWeight: 600,
 });
 
 export default typography;
