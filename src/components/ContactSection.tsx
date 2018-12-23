0e3297e86c35cb5a2278adb86448a697b0b0c2a9
Kristóf Poduszló <kripod@protonmail.com>
2018-12-23 01:44:07 +0100

    Add social media icons to the contact section

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,6 +1,7 @@
 import { graphql, StaticQuery } from 'gatsby';
 import React from 'react';
 import { Heading, Link, Text } from 'rebass';
+import { Behance, Facebook, Instagram } from 'styled-icons/fa-brands';
 
 const ContactSection = () => (
   <Text as="section" textAlign="center">
@@ -12,24 +13,43 @@ const ContactSection = () => (
           site {
             siteMetadata {
               email
+              siteBehanceURL
+              siteFacebookURL
+              siteInstagramURL
             }
           }
         }
       `}
       render={data => (
-        <Text>
-          Van egy projekted számunkra?
-          <br />
-          Küldj egy e-mailt a{' '}
-          <Text
-            as={Link}
-            href={`mailto:${data.site.siteMetadata.email}`}
-            fontWeight="bold"
-          >
-            {data.site.siteMetadata.email}
-          </Text>{' '}
-          címre!
-        </Text>
+        <>
+          <Text my={4} lineHeight={0}>
+            <Link href={data.site.siteMetadata.siteFacebookURL}>
+              <Facebook title="Facebook" size="2em" />
+            </Link>
+
+            <Link href={data.site.siteMetadata.siteInstagramURL} mx={4}>
+              <Instagram title="Instagram" size="2em" />
+            </Link>
+
+            <Link href={data.site.siteMetadata.siteBehanceURL}>
+              <Behance title="Behance" size="2em" />
+            </Link>
+          </Text>
+
+          <Text as="p">
+            Van egy projekted számunkra?
+            <br />
+            Küldj egy e-mailt a{' '}
+            <Text
+              as={Link}
+              href={`mailto:${data.site.siteMetadata.email}`}
+              fontWeight="bold"
+            >
+              {data.site.siteMetadata.email}
+            </Text>{' '}
+            címre!
+          </Text>
+        </>
       )}
     />
   </Text>
