19a46506cdef2795353df883c6575db15c0d888f
Kristóf Poduszló <kripod@protonmail.com>
2018-12-23 23:53:12 +0100

    Start working on responsive typography

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,10 +1,27 @@
 import Typography from 'typography';
+import theme from './theme';
 
 const typography = new Typography({
   headerFontFamily: ['Roboto Mono', 'monospace'],
   bodyFontFamily: ['Montserrat', 'system-ui', 'sans-serif'],
   headerWeight: 500,
   boldWeight: 600,
+
+  // Responsive typography
+  // Source: https://github.com/KyleAMathews/typography.js/issues/19#issuecomment-327630129
+  overrideStyles: () => ({
+    [`@media (min-width: ${theme.breakpoints[1]}px)`]: {
+      html: {
+        fontSize: '125%',
+      },
+    },
+
+    [`@media (min-width: ${theme.breakpoints[3]}px)`]: {
+      html: {
+        fontSize: '150%',
+      },
+    },
+  }),
 });
 
 export default typography;
