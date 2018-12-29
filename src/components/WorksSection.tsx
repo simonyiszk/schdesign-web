4b7a27fff48ed744d880fcb7d1dde0b78d6ac80d
Kristóf Poduszló <kripod@protonmail.com>
2018-12-29 14:30:18 +0100

    Make vertical padding of the works section header bigger

--- src/components/WorksSection.tsx
+++ src/components/WorksSection.tsx
@@ -6,7 +6,7 @@ import Section, { Props } from './Section';
 
 const WorksSection = (props: Props) => (
   <Section {...props}>
-    <Heading id="works" textAlign="center" py={[3, 4]}>
+    <Heading id="works" textAlign="center" py={4}>
       Munkáink
     </Heading>
 
