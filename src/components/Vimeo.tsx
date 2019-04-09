a4b5ae6e22f2b630ca8ea9f6a03e191505f0ff4d
shixiedesign <shixie@ibm.com>
2019-04-09 11:57:05 -0500

    video component working

--- src/components/Vimeo.tsx
+++ src/components/Vimeo.tsx
@@ -10,8 +10,7 @@ const Vimeo = (props: Props) => (
 <Box
   mb={4}
   >
-  <Text> Does this work? </Text>
-  <Iframe url="https://player.vimeo.com/video/329030713"
+  <Iframe url={"https://player.vimeo.com/video/"+props.vimeoid}
     className="myClassname"
     position="relative"
     webkitallowfullscreen
