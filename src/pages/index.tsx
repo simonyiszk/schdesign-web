dc817169dabd97c6bd4f7ec221ab5da9257aeeaa
shixiedesign <shixie@ibm.com>
2019-05-12 22:27:35 -0500

    clean2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import AboutSection from '../components/AboutSection';
-import CourseSection from '../components/CourseSection';
-import ContactSection from '../components/ContactSection';
 import Layout from '../components/Layout';
 import CloudVR from '../components/CloudVR';
 import Arvrdg from '../components/Arvrdg';
@@ -9,8 +6,6 @@ import Carbon from '../components/Carbon';
 
 const IndexPage = () => (
   <Layout>
-    {/* <AboutSection /> */}
-    {/* <CourseSection /> */}
     <CloudVR />
     <Arvrdg />
     <Carbon />
