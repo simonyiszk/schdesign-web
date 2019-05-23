55eb16323f6b9ec508b507e3fcb670100ae70371
Kristóf Poduszló <kripod@protonmail.com>
2019-05-23 18:32:51 +0200

    Make course section component commented out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
+// import CourseSection from '../components/CourseSection';
 import ContactSection from '../components/ContactSection';
 import Layout from '../components/Layout';
 import WorksSection from '../components/WorksSection';
@@ -7,6 +8,7 @@ import WorksSection from '../components/WorksSection';
 const IndexPage = () => (
   <Layout>
     <AboutSection />
+    {/* <CourseSection /> */}
     <WorksSection variant="dark" />
     <ContactSection my={[4, 5]} />
   </Layout>
