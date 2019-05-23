74dedfe5dc37edcb6493222fc2978293dfd2a27f
Kristóf Poduszló <kripod@protonmail.com>
2019-05-23 18:29:07 +0200

    Remove course section from the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
-import CourseSection from '../components/CourseSection';
 import ContactSection from '../components/ContactSection';
 import Layout from '../components/Layout';
 import WorksSection from '../components/WorksSection';
@@ -8,7 +7,6 @@ import WorksSection from '../components/WorksSection';
 const IndexPage = () => (
   <Layout>
     <AboutSection />
-    <CourseSection />
     <WorksSection variant="dark" />
     <ContactSection my={[4, 5]} />
   </Layout>
