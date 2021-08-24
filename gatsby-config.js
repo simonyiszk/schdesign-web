fea3613a103089b786b1489803f51344294a854d
Andris Borbás <andris.borbas@gmail.com>
2021-08-25 00:37:09 +0200

    Add redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,5 +66,6 @@ module.exports = {
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
+		`gatsby-plugin-meta-redirect`, // make sure to put last in the array
 	],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -15,3 +15,53 @@ exports.onCreateWebpackConfig = ({ actions }) => {
 		},
 	});
 };
+
+exports.createPages = async ({ graphql, actions }) => {
+	const { createRedirect } = actions;
+	createRedirect({
+		fromPath: "/drive",
+		toPath:
+			"https://drive.google.com/drive/u/1/folders/0B8rKwHW6MJ1QeEFVVXc5eGVzUzA",
+		isPermanent: true,
+		redirectInBrowser: true,
+	});
+	createRedirect({
+		fromPath: "/trello",
+		toPath: "https://trello.com/b/wpWdmPYG/schdesign-2021-ősz",
+		isPermanent: true,
+	});
+	createRedirect({
+		fromPath: "/figma",
+		toPath: "https://www.figma.com/files/team/796103961140349650/schdesign",
+		isPermanent: true,
+	});
+	createRedirect({
+		fromPath: "/logotar",
+		toPath: "https://logotar.schdesign.hu",
+		isPermanent: true,
+	});
+	createRedirect({
+		fromPath: "/proj",
+		toPath:
+			"https://drive.google.com/drive/u/1/folders/0B8rKwHW6MJ1QVEd5N240dlJSU2s",
+		isPermanent: true,
+	});
+	createRedirect({
+		fromPath: "/919",
+		toPath:
+			"https://calendar.google.com/calendar/embed?src=jidr5qaro7d4l7mfvf83d33o90@group.calendar.google.com&ctz=Europe/Budapest&pli=1",
+		isPermanent: true,
+	});
+	createRedirect({
+		fromPath: "/simonyiarculat",
+		toPath:
+			"https://drive.google.com/drive/u/2/folders/0B5vSboSK4bYaMDd2MVB1U2NhNTA",
+		isPermanent: true,
+	});
+	createRedirect({
+		fromPath: "/roadmap",
+		toPath: "https://roadmap.schdesign.hu",
+		isPermanent: true,
+	});
+	// Create pages here
+};
