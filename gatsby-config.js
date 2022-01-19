88e1f87fbf6b992c66710d8388648401df8f0e15
Gergő Radeczki <g.radeczki@gmail.com>
2022-01-19 15:11:16 +0100

    added GTM added htaccess redirect

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,86 @@ const siteMetadata = {
 module.exports = {
 	siteMetadata,
 	plugins: [
+		/** Gatsby Plugin Htaccess */
+		{
+			resolve: "gatsby-plugin-htaccess",
+			options: {
+				redirect: [
+					"RewriteRule ^not-existing-url/?$ /existing-url [R=301,L,NE]",
+					{
+						from: "/drive",
+						to: "https://drive.google.com/drive/u/1/folders/0B8rKwHW6MJ1QeEFVVXc5eGVzUzA",
+					},
+					{
+						from: "/trello",
+						to: "https://trello.com/b/wpWdmPYG/schdesign-2021-ősz",
+					},
+					{
+						from: "/figma",
+						to: "https://www.figma.com/files/team/796103961140349650/schdesign",
+					},
+					{
+						from: "/logotar",
+						to: "https://logotar.schdesign.hu",
+					},
+					{
+						from: "/roadmap",
+						to: "https://roadmap.schdesign.hu",
+					},
+					{
+						from: "/proj",
+						to: "https://drive.google.com/drive/u/1/folders/0B8rKwHW6MJ1QVEd5N240dlJSU2s",
+					},
+					{
+						from: "/919",
+						to: "https://calendar.google.com/calendar/embed?src=jidr5qaro7d4l7mfvf83d33o90@group.calendar.google.com&ctz=Europe/Budapest&pli=1",
+					},
+					{
+						from: "/simonyiarculat",
+						to: "https://drive.google.com/drive/u/2/folders/0B5vSboSK4bYaMDd2MVB1U2NhNTA",
+					},
+					{
+						from: "/szmsz",
+						to: "https://drive.google.com/file/d/1Jpa6VdPs5PPQm16w6aOW_00RuA8cxYST/view?usp=sharing",
+					},
+				],
+			},
+		},
+
+		/** Google Tag Manager */
+		{
+			resolve: "gatsby-plugin-google-tagmanager",
+			options: {
+				id: "GTM-KBLM6CQ",
+
+				/// Include GTM in development.
+				///
+				/// Defaults to false meaning GTM will only be loaded in production.
+				includeInDevelopment: false,
+
+				/// datalayer to be set before GTM is loaded
+				/// should be an object or a function that is executed in the browser
+				///
+				/// Defaults to null
+				defaultDataLayer: { platform: "gatsby" },
+
+				/// Specify optional GTM environment details.
+				// gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+				// gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+				// dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+				/// Name of the event that is triggered
+				/// on every Gatsby route change.
+				///
+				/// Defaults to gatsby-route-change
+				// routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+				// Defaults to false
+				enableWebVitalsTracking: true,
+				// Defaults to https://www.googletagmanager.com
+				// selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
+			},
+		},
+
 		/** Contentful types */
 		{
 			resolve: `gatsby-plugin-typegen`,
