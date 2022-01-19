23b8e513a1549d504f792b68b957a51bd3f7fdce
Gergő Radeczki <g.radeczki@gmail.com>
2022-01-19 15:36:28 +0100

    Added GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,40 @@ const siteMetadata = {
 module.exports = {
 	siteMetadata,
 	plugins: [
+		/** Google Tag Manager */
+		{
+			resolve: "gatsby-plugin-google-tagmanager",
+			options: {
+				id: "GTM-KBLM6CQ",
+
+				/// Include GTM in development.
+				///
+				/// Defaults to false meaning GTM will only be loaded in production.
+				includeInDevelopment: false,
+
+				/// datalayer to be set before GTM is loaded
+				/// should be an object or a function that is executed in the browser
+				///
+				/// Defaults to null
+				defaultDataLayer: { platform: "gatsby" },
+
+				/// Specify optional GTM environment details.
+				// gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+				// gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+				// dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+				/// Name of the event that is triggered
+				/// on every Gatsby route change.
+				///
+				/// Defaults to gatsby-route-change
+				// routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+				// Defaults to false
+				enableWebVitalsTracking: true,
+				// Defaults to https://www.googletagmanager.com
+				// selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
+			},
+		},
+
 		/** Contentful types */
 		{
 			resolve: `gatsby-plugin-typegen`,
