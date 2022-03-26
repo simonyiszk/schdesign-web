feae501970377bce07a698ed95208fc5fc4d7aeb
Andris Borbás <andris.borbas@gmail.com>
2022-03-26 16:48:18 +0100

    Revert idk whats this

--- src/components/memberCard/MemberCard.tsx
+++ src/components/memberCard/MemberCard.tsx
@@ -9,7 +9,7 @@ export type MemberCardProps = {
 	title?: string;
 	imageData?: ImageDataLike | null;
 	isOld?: boolean;
-	isCurrentLeadership?: 
+	isCurrentLeadership?: boolean;
 } & React.HTMLProps<HTMLDivElement>;
 
 export function MemberCard({
@@ -18,12 +18,16 @@ export function MemberCard({
 	title,
 	imageData,
 	isOld,
+	isCurrentLeadership,
 	...restProps
 }: MemberCardProps) {
 	const image = imageData && getImage(imageData);
 
 	function getTitle() {
-		if (title != null) {
+		if (title && !isCurrentLeadership) {
+			return `Volt-${title}`;
+		}
+		if (title) {
 			return title;
 		}
 		return isOld ? "Öregtag" : "Tag";
@@ -32,30 +36,32 @@ export function MemberCard({
 	return (
 		<figure
 			className={clsx(
-				"flex flex-col justify-center p-4 max-w-xs text-center bg-white rounded-2xl shadow-2xl transform hover:scale-105 transition ease-in-out",
+				"group flex flex-col justify-center p-4 max-w-xs text-center bg-white rounded-2xl shadow-2xl",
 				restProps.className,
 			)}
 		>
-			{image ? (
-				<GatsbyImage
-					image={image}
-					alt={`${name} arcképe`}
-					imgClassName="mb-2 h-64 rounded-2xl object-cover overflow-hidden"
-				/>
-			) : (
-				<StaticImage
-					src="../../assets/images/blank.png"
-					placeholder="blurred"
-					alt="Hiányzó arckép"
-					imgClassName="mb-2 h-64 rounded-2xl object-cover overflow-hidden"
-				/>
-			)}
+			<div className="mb-2 h-64 rounded-2xl object-cover overflow-hidden">
+				{image ? (
+					<GatsbyImage
+						image={image}
+						alt={`${name} arcképe`}
+						className="transform group-hover:scale-105 transition duration-300 ease-in-out"
+					/>
+				) : (
+					<StaticImage
+						src="../../assets/images/blank.png"
+						placeholder="blurred"
+						alt="Hiányzó arckép"
+						className="transform group-hover:scale-105 transition duration-300 ease-in-out"
+					/>
+				)}
+			</div>
 
 			<h4 className="mb-2 text-lg font-medium border-b-2 border-primary">
 				{name}
 			</h4>
 
-			{email && (
+			{email && isCurrentLeadership && (
 				<a
 					className="mb-0.5 hover:text-primary text-sm"
 					href={`mailto:${email}`}
