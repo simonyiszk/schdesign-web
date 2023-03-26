cddc77ec193652032612b0fff970f1faa011fcd0
Miklos Daniel <daniel.miklos0@gmail.com>
2023-03-26 21:47:12 +0200

    fix vercel env var

--- src/utils/contentful.ts
+++ src/utils/contentful.ts
@@ -19,11 +19,11 @@ import type {
 const client = createClient({
 	space: process.env.CONTENTFUL_SPACE_ID ?? "ErrorNoSpaceID",
 	accessToken:
-		(process.env.VERCEL_ENV === "production"
+		(process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
 			? process.env.CONTENTFUL_ACCESS_TOKEN
 			: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN) ?? "ErrorNoAccessToken",
 	host:
-		process.env.VERCEL_ENV === "production"
+		process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
 			? "cdn.contentful.com"
 			: "preview.contentful.com",
 }).withoutUnresolvableLinks;
