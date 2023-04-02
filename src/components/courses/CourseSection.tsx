a8db78d59b3fb28612f2b0aa0d6745f5fa3fa0a6
Miklos Daniel <46491285+perryd01@users.noreply.github.com>
2023-04-02 23:15:04 +0200

    hide past courses after 12 hours (#32)

--- src/components/courses/CourseSection.tsx
+++ src/components/courses/CourseSection.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import type { GetCoursesReturnType } from "@/utils/contentful";
 
 import { Course } from "./Course";
@@ -7,9 +9,27 @@ export type CourseSectionProps = {
 };
 
 export function CourseSection({ courses }: CourseSectionProps) {
+	const [renderedCourses, setRenderedCourses] = useState<GetCoursesReturnType>(
+		[],
+	);
+
+	const dayInMilliseconds = 1000 * 60 * 60 * 24;
+
+	useEffect(() => {
+		const now = new Date();
+		setRenderedCourses(
+			courses.filter((course) =>
+				course.fields.date
+					? new Date(course.fields.date).getTime() >
+					  now.getTime() - dayInMilliseconds / 2
+					: true,
+			),
+		);
+	}, []);
+
 	return (
 		<div className="flex flex-wrap justify-center gap-8">
-			{courses.map((course) => (
+			{renderedCourses.map((course) => (
 				<Course
 					key={course.fields.title}
 					mdxSource={course.mdxSource}
