b7e4d6e230f8da84c87530f4e6c81ad65695a339
Miklos Daniel <daniel.miklos0@gmail.com>
2024-02-18 13:53:31 +0100

    remove client component related calls in courseSection

--- src/components/courses/CourseSection.tsx
+++ src/components/courses/CourseSection.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 import type { GetCoursesReturnType } from "@/utils/contentful";
 
 import { Course } from "./Course";
@@ -11,27 +7,9 @@ export type CourseSectionProps = {
 };
 
 export function CourseSection({ courses }: CourseSectionProps) {
-	const [renderedCourses, setRenderedCourses] = useState<GetCoursesReturnType>(
-		[],
-	);
-
-	const dayInMilliseconds = 1000 * 60 * 60 * 24;
-
-	useEffect(() => {
-		const now = new Date();
-		setRenderedCourses(
-			courses.filter((course) =>
-				course.fields.date
-					? new Date(course.fields.date).getTime() >
-					  now.getTime() - dayInMilliseconds / 2
-					: true,
-			),
-		);
-	}, []);
-
 	return (
 		<div className="flex flex-wrap justify-center gap-8">
-			{renderedCourses.map((course) => (
+			{courses.map((course) => (
 				<Course
 					key={course.fields.title}
 					mdxSource={course.mdxSource}
