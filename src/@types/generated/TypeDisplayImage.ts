3a909c3102454de58abb320d1611c1baf0f932b1
Miklos Daniel <daniel.miklos0@gmail.com>
2024-12-22 16:56:51 +0100

    regenerate contentful types

--- src/@types/generated/TypeDisplayImage.ts
+++ src/@types/generated/TypeDisplayImage.ts
@@ -1,10 +1,16 @@
-import type { Asset, Entry, EntryFields } from "contentful";
+import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
 
-export type TypeDisplayImageFields = {
-    title: EntryFields.Symbol;
-    author: EntryFields.Symbol;
-    image: Asset;
-    createdAt: EntryFields.Date;
+export interface TypeDisplayImageFields {
+    title: EntryFieldTypes.Symbol;
+    author: EntryFieldTypes.Symbol;
+    image: EntryFieldTypes.AssetLink;
+    createdAt: EntryFieldTypes.Date;
 }
 
-export type TypeDisplayImage = Entry<TypeDisplayImageFields>;
+export type TypeDisplayImageSkeleton = EntrySkeletonType<TypeDisplayImageFields, "displayImage">;
+export type TypeDisplayImage<Modifiers extends ChainModifiers, Locales extends LocaleCode = LocaleCode> = Entry<TypeDisplayImageSkeleton, Modifiers, Locales>;
+export type TypeDisplayImageWithoutLinkResolutionResponse = TypeDisplayImage<"WITHOUT_LINK_RESOLUTION">;
+export type TypeDisplayImageWithoutUnresolvableLinksResponse = TypeDisplayImage<"WITHOUT_UNRESOLVABLE_LINKS">;
+export type TypeDisplayImageWithAllLocalesResponse<Locales extends LocaleCode = LocaleCode> = TypeDisplayImage<"WITH_ALL_LOCALES", Locales>;
+export type TypeDisplayImageWithAllLocalesAndWithoutLinkResolutionResponse<Locales extends LocaleCode = LocaleCode> = TypeDisplayImage<"WITHOUT_LINK_RESOLUTION" | "WITH_ALL_LOCALES", Locales>;
+export type TypeDisplayImageWithAllLocalesAndWithoutUnresolvableLinksResponse<Locales extends LocaleCode = LocaleCode> = TypeDisplayImage<"WITHOUT_UNRESOLVABLE_LINKS" | "WITH_ALL_LOCALES", Locales>;
--- src/@types/generated/TypeParagraph.ts
+++ src/@types/generated/TypeParagraph.ts
@@ -1,9 +1,15 @@
-import type { Entry, EntryFields } from "contentful";
+import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
 
-export type TypeParagraphFields = {
-    title: EntryFields.Symbol;
-    order: EntryFields.Integer;
-    content: EntryFields.Text;
+export interface TypeParagraphFields {
+    title: EntryFieldTypes.Symbol;
+    order: EntryFieldTypes.Integer;
+    content: EntryFieldTypes.Text;
 }
 
-export type TypeParagraph = Entry<TypeParagraphFields>;
+export type TypeParagraphSkeleton = EntrySkeletonType<TypeParagraphFields, "paragraph">;
+export type TypeParagraph<Modifiers extends ChainModifiers, Locales extends LocaleCode = LocaleCode> = Entry<TypeParagraphSkeleton, Modifiers, Locales>;
+export type TypeParagraphWithoutLinkResolutionResponse = TypeParagraph<"WITHOUT_LINK_RESOLUTION">;
+export type TypeParagraphWithoutUnresolvableLinksResponse = TypeParagraph<"WITHOUT_UNRESOLVABLE_LINKS">;
+export type TypeParagraphWithAllLocalesResponse<Locales extends LocaleCode = LocaleCode> = TypeParagraph<"WITH_ALL_LOCALES", Locales>;
+export type TypeParagraphWithAllLocalesAndWithoutLinkResolutionResponse<Locales extends LocaleCode = LocaleCode> = TypeParagraph<"WITHOUT_LINK_RESOLUTION" | "WITH_ALL_LOCALES", Locales>;
+export type TypeParagraphWithAllLocalesAndWithoutUnresolvableLinksResponse<Locales extends LocaleCode = LocaleCode> = TypeParagraph<"WITHOUT_UNRESOLVABLE_LINKS" | "WITH_ALL_LOCALES", Locales>;
